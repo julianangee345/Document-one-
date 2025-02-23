4dcb279b8572e243c54d67413037848d14c19fcd
julianangee345 <ngeejuliana@gmail.com>
2025-02-23 10:27:52 +0300

    Add files via upload

---
+++ compound_interest.C
@@ -0,0 +1,24 @@
+//PROGRAM TO CALCULATE COMPOUBD INTEREST
+/*
+The prigram will ask the user to input the following:
+principle amount
+interest rate
+*/
+#include <stdio.h>
+#include <math.h>
+int main() 
+{
+    float principle, rate, time, compound_interest, amount;
+    int n; 
+    printf("Enter the principal amount (Ksh): \n");
+    scanf("%f", &principle);
+    printf("Enter the rate (%%): \n");
+    scanf("%f", &rate);
+    printf("Enter time in years:\n ");
+    scanf("%f", &time);
+    printf("Number of times compounded yearly:\n ");
+    scanf("%d", &n);
+    compound_interest = principle * pow((1 + rate / (100 * n)), (n * time));
+    printf("Compound Interest: Ksh %.2f\n", compound_interest);
+    return 0;
+}
