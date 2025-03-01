a2f29f7bbc4e579e75947f094d998cdf6f71f932
julianangee345 <ngeejuliana@gmail.com>
2025-03-01 14:58:18 +0300

    Add files via upload

---
+++ overdue_library_books.c
@@ -0,0 +1,44 @@
+//program to calculate the fine for overdue library books
+/*
+Author:JULIANA MATHENGE 
+ADM: D33-5023-2023
+DATE:01/03/2025
+*/
+#include <stdio.h>
+
+int main() {
+  int bookID, dueDate, returnDate, daysOverdue, fineRate, fineAmount;
+
+  // Input from user
+  printf("Enter Book ID: ");
+  scanf("%d", &bookID);
+  printf("Enter Due Date: ");
+  scanf("%d", &dueDate);
+  printf("Enter Return Date: ");
+  scanf("%d", &returnDate);
+  
+  // Calculate days overdue
+  daysOverdue = returnDate - dueDate;
+
+  // Determine fine rate
+  if (daysOverdue <= 7) {
+  fineRate = 20;
+  } else if (daysOverdue <= 14) {
+  fineRate = 50;
+  } else {
+  fineRate = 100;
+  }
+
+  // Calculate fine amount
+  fineAmount = daysOverdue * fineRate;
+
+  // Display results
+  printf("Book ID: %d\n", bookID);
+  printf("Due Date: %d\n", dueDate);
+  printf("Return Date: %d\n", returnDate);
+  printf("Days Overdue: %d\n", daysOverdue);
+  printf("Fine Rate: %d\n", fineRate);
+  printf("Fine Amount: %d\n", fineAmount);
+
+  return 0;
+}
