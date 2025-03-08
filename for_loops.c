429afbea78df3c0836a0aa6cb986c72745d64ff8
julianangee345 <ngeejuliana@gmail.com>
2025-03-08 16:08:47 +0300

    Add files via upload

---
+++ for_loops.c
@@ -0,0 +1,18 @@
+/*
+For loops
+Author:Juliana mathenge
+adm: D33-5023-2023
+date: 8-03-2025
+*/
+#include <stdio.h>
+
+int main() {
+   int i=1;
+   //for(start; stop; step)
+   for(i=1; i<=100; i=i+1){
+       
+       
+        printf("number is :%d, and  cube of the %d is:%d\n",i,i, (i*i*i)) ; 
+  }
+    return 0;
+}
